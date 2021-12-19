39698473a1ad90e0f8724ae13b774e7c8ac29b68
areej <areej101223@gmail.com>
2021-12-19 16:40:08 +0300

    Menu App (Bonus)

---
+++ app/src/main/java/com/example/menuapp/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.menuapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.example.menuapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.Home -> startActivity(Intent(this,Home::class.java))
+            R.id.About -> startActivity(Intent(this,About::class.java))
+            R.id.Help -> startActivity(Intent(this,Help::class.java))
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+}
